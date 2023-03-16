77fbdc69729c96842d7dba5f280148cfde01a9f6
gmichaud <gmichaud@campus-eni.fr>
2023-03-16 16:31:26 +0100

    TP Module 5 : Bucket-List : Données et Doctrine

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Wish;
+use App\Repository\WishRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,20 +13,45 @@ use Symfony\Component\Routing\Annotation\Route;
 class WishController extends AbstractController
 {
     #[Route('list', name: 'list')]
-    public function list(): Response
+    public function list(WishRepository $wishRepository): Response
     {
+        $wishes = $wishRepository->findBy(['isPublished'=>true], ['dateCreated'=>'DESC']);
+
+
         return $this->render('wish/list.html.twig', [
-            // todo : liste des souhaits en BDD
+            'wishes' => $wishes
         ]);
     }
 
     #[Route('detail/{id}', name: 'detail')]
-    public function detail(int $id): Response
+    public function detail(int $id, WishRepository $wishRepository): Response
     {
+
+        $wish = $wishRepository->find($id);
+
         return $this->render('wish/detail.html.twig', [
-            // todo : un souhait en BDD
+            'wish' => $wish
         ]);
     }
+
+
+    #[Route('demo', name: 'demo')]
+    public function demo(EntityManagerInterface $entityManager){
+
+        $wish = new Wish();
+        $wish->setTitle('Faire le tour du monde');
+        $wish->setAuthor('moi');
+        $wish->setDescription('en bateau');
+        $wish->setDateCreated(new \DateTime());
+        $wish->setIsPublished(true);
+
+        $entityManager->persist($wish);
+        $entityManager->flush();
+
+        return $this->render('wish/list.html.twig');
+
+    }
+
 
 
 }
